694f4797f5de4f64413459fa37464c48bf88ccf8
shaharbn <shahar.barnatan@gmail.com>
2025-03-18 19:09:40 +0200

    final project

--- main.py
+++ main.py
@@ -17,12 +17,39 @@ def main():
             months = int(input("months: "))
             return_yield = buy_and_sell_in_x_month(buying_price, selling_price, months)
             print(f"return {return_yield}%")
+        elif user_input == "3":
+            buying_price = int(input("buying price: "))
+            rent = int(input("rent: "))
+            return_yield = buy_and_rent_yield(buying_price, rent)
+            print(f"return {return_yield}%")
+        elif user_input == "4":
+            hon_azmi = int(input("hon azmi: "))
+            mashcanta = int(input("mashcanta: "))
+            ribit = float(input("ribit: "))
+            rent = int(input("rent: "))
+            return_yield = minuf_yield(hon_azmi, mashcanta, ribit, rent)
+            print(f"return {return_yield}%")
+        elif user_input == "5":
+            rent = int(input("rent: "))
+            hivun_rate = float(input("hivun rate: "))
+            price = hivun(rent, hivun_rate)
+            print(f"price: {price}")
         elif user_input == 'q':
             print("good bye")
+        else:
+            print("unkwown command")
 
 def menu():
+    print("======================================================")
     print("[1] buy property in Y money and make X money in a year")
     print("[2] buy and sell in X months")
+    print("[3] buy and rent")
+    print("------------------------------------------------------")
+    print("[4] return with minuf")
+    print("------------------------------------------------------")
+    print("[5] hivun")
+    print("------------------------------------------------------")
     print("[q] exit")
+    print("======================================================")
     
 main()
